f5cc734efa0d58e345cabc332cdadfcfeedb0718
Damashdam <danielburlakov@gmail.com>
2024-06-05 18:56:23 +0300

    login/RidesList\

---
+++ assets/js/allRides.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('data/data.json')
+        .then(response => response.json())
+        .then(data => {
+            const rides = data.rides;
+            const rideRow = document.querySelector('.rideRow');
+
+            rides.forEach(ride => {
+                const rideElement = document.createElement('div');
+                rideElement.classList.add('ride');
+
+                rideElement.innerHTML = `
+                    <div class="ride-details">
+                        <p><strong>From:</strong> ${ride.from}</p>
+                        <p><strong>To:</strong> ${ride.to}</p>
+                        <p><strong>Free Seats:</strong> ${ride.free_seats}</p>
+                        <p><strong>Trunk Available:</strong> ${ride.is_trunk ? 'Yes' : 'No'}</p>
+                        <p><strong>Date of Ride:</strong> ${ride.date_of_ride}</p>
+                        <p><strong>Time of Ride:</strong> ${ride.time_of_ride}</p>
+                        <button class="takeRideButton" data-ride-id="${ride.id}">Take Ride</button>
+                        <p ><strong>Free Seats</strong></p>
+
+                        <input placeholder ="${ride.free_seats}"></input>
+                    </div>
+                `;
+
+                rideRow.appendChild(rideElement);
+            });
+
+            const buttons = document.querySelectorAll('.takeRideButton');
+            buttons.forEach(button => {
+                const rideId = button.dataset.rideId;
+                if (localStorage.getItem(`ride_${rideId}_clicked`) === 'true') {
+                    button.style.backgroundColor = 'green';
+                    button.disabled = true;
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Error loading rides:', error);
+        });
+
+    document.querySelector('.rideRow').addEventListener('click', function (event) {
+        if (event.target.classList.contains('takeRideButton')) {
+            event.preventDefault(); // Prevent default form submission behavior
+            const button = event.target;
+            button.style.backgroundColor = 'green';
+            button.disabled = true;
+            const rideId = button.dataset.rideId;
+            localStorage.setItem(`ride_${rideId}_clicked`, 'true');
+            addOrder(rideId);
+        }
+    });
+
+    function addOrder(rideId) {
+        const userId = localStorage.getItem('isLoggedIn');
+        const newOrder = {
+            id: generateUniqueId(),
+            ride_id: rideId,
+            user_id: userId,
+            status: 'true'
+        };
+
+        fetch('http://localhost:8000/orders', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newOrder)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Order added successfully:', data);
+                // You can perform further actions upon successful addition of the order if needed
+            })
+            .catch(error => {
+                console.error('Error adding order:', error);
+            });
+    }
+
+    function generateUniqueId() {
+        return Math.random().toString(36).substr(2, 9);
+    }
+});
---
+++ assets/js/loginScript.js
@@ -0,0 +1,30 @@
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('errorMessage');
+
+    // Fetch data from data.json file
+    fetch('data/data.json')
+        .then(response => response.json())
+        .then(data => {
+            // Check if username exists in the data
+            const user = data.users.find(user => user.id === username);
+
+            if (user && user.password === password) {
+                errorMessage.textContent = "Login successful!";
+                errorMessage.style.color = "green";
+                localStorage.setItem('isLoggedIn', user.id);
+                window.location.href = './index.html';
+            } else {
+                errorMessage.textContent = "Invalid username or password.";
+                errorMessage.style.color = "red";
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            errorMessage.textContent = "Error fetching data.";
+            errorMessage.style.color = "red";
+        });
+});
