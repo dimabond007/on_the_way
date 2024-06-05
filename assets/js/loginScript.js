fa4d74ec921875423192aaa11256a8a4ed79c0c3
omrinelken <omri.nelken@gmail.com>
2024-06-06 01:01:57 +0300

    new

--- assets/js/loginScript.js
+++ assets/js/loginScript.js
@@ -6,17 +6,17 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
     const errorMessage = document.getElementById('errorMessage');
 
     // Fetch data from data.json file
-    fetch('data/data.json')
+    fetch('http://localhost:8000/user/' + username)
         .then(response => response.json())
         .then(data => {
             // Check if username exists in the data
-            const user = data.users.find(user => user.id === username);
+            const user = data;
 
             if (user && user.password === password) {
                 errorMessage.textContent = "Login successful!";
                 errorMessage.style.color = "green";
                 localStorage.setItem('isLoggedIn', user.id);
-                window.location.href = './index.html';
+                window.location.href = './welcome_page.html';
             } else {
                 errorMessage.textContent = "Invalid username or password.";
                 errorMessage.style.color = "red";
